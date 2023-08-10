0532857d944809561564c676e23290261d3f05ce
mammut001 <141458085+mammut001@users.noreply.github.com>
2023-08-09 23:07:06 -0400

    update plan.js
    add plan.css

--- my-react-app/src/component/Plan.js
+++ my-react-app/src/component/Plan.js
@@ -1,11 +1,33 @@
 import React from "react"
+import "./Plan.css"
+const daysOfWeek = {
+    "Monday": "星期一",
+    "Tuesday": "星期二",
+    "Wednesday": "星期三",
+    "Thursday": "星期四",
+    "Friday": "星期五",
+    "Saturday": "星期六",
+    "Sunday": "星期日"
+  };
 
+
+
+const weeksButtons = () => {
+    return (
+        Object.values(daysOfWeek).map((day,index) =>(
+            <div key={index}>
+                <button className="weeks-button">{day}</button>
+                <br/>
+            </div>
+            
+        ))
+        
+    )
+}
 function Plan(){
     return(
         <div>
-            <h1>
-                create plan111
-            </h1>
+            {weeksButtons()}
         </div>
     )
 }
