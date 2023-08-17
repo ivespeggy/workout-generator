529eda9364326b084f19b3ab2bdc22e746f39fe8
HaonanZhang101045166 <59615110+HaonanZhang101045166@users.noreply.github.com>
2023-08-17 15:27:09 -0400

    Reorganize the structure and set a univeral home for future general purpose

--- my-react-app/src/component/Home.js
+++ my-react-app/src/component/Home.js
@@ -1,51 +1,11 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // 导入 Link 组件
-import musclesData from '../data/muscles';
+import React from 'react';
 import "./Home.css"
 
 export default function Home() {
-  const navigate = useNavigate();
-  const [clickState, setClickState] = useState(Array.from({ length: 13 }, () => 0)); // 初始化 clickState
-  const [form, setForm]=useState({});
-
-  const ClickAction = (clickId) => {
-    // 更新 clickState 逻辑
-    const updatedState = [...clickState];
-    updatedState[clickId] = updatedState[clickId] === 0 ? 1 : 0;
-    setClickState(updatedState);
-  };
-  const ButtonOnClick = () =>{
-    console.log("You clicked submit")
-    //Switch to plan 
-    navigate('/Plan')
-  }
 
   return (
     <div>
-      <header className="App-header">
-        <h1>牛逼哄哄的title!!!</h1>
-        <ul>
-          {Object.keys(musclesData).map((key) => {
-            const id = musclesData[key].id;
-            const name_cn = musclesData[key].name_cn;
-            const name_en = musclesData[key].name_en;
-            const training_moves = musclesData[key].training_moves;
-            return (
-              <li className="ul" key={id} onClick={() => ClickAction(id)}>
-                <strong>{name_cn}</strong> <strong>{name_en}</strong>
-                {clickState[id] === 1 && (
-                  <ul>
-                    {training_moves.map((move, index) => (
-                      <li key={index}>{move}</li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            );
-          })}
-        </ul>
-          <button className="submit-button" type="submit" onClick={ButtonOnClick}>点击创建您自己的训练计划吧！！</button>
-      </header>
+      Real Home
     </div>
   );
 }
