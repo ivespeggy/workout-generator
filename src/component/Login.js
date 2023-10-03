09bc5cb76e504f981a04a717421dcfee45886206
HaonanZhang101045166 <59615110+HaonanZhang101045166@users.noreply.github.com>
2023-10-03 19:04:16 -0400

    Using axios to consume /users/login Successfully tested scenarios, introduced useRef and useContext

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,15 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
 import "./Login.css";
 import { Form, InputGroup, Button } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
+import AuthContext from "../context/AuthProvider";
+import axios from "../api/axios";
+
+const LOGIN_URL = "/users/login";
 
 export default function Login() {
+  const { setAuth } = useContext(AuthContext);
+  const userRef = useRef();
+  const errRef = useRef();
   const navigate = useNavigate();
   const [loginState, setLoginState] = useState({
     username: "",
     password: "",
   });
-  const [authorized, setAuthorized] = useState(false);
+  // const [authorized, setAuthorized] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
   const handleChange = (e) => {
     setLoginState({
       ...loginState,
@@ -17,32 +25,82 @@ export default function Login() {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(loginState);
+
+    try {
+      const basicAuthCredentials = btoa(
+        loginState.username + ":" + loginState.password
+      );
+      const response = await axios.post(LOGIN_URL, JSON.stringify(loginState), {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + basicAuthCredentials,
+        },
+        // 这样就带着cookie了 如果后端CORS设置时.allowedOrigin("*")就会报错
+        // withCredentials: true,
+      });
+
+      const accessToken = response?.data?.data.token;
+      const roles = response?.data?.data.userInfo.roles;
+      setAuth({ roles, accessToken });
+      // console.log(JSON.stringify(roles));
+      setLoginState({
+        username: "",
+        password: "",
+      });
+      navigate("/");
+    } catch (error) {
+      if (!error?.response) {
+        setErrorMsg("No Server Response");
+      } else {
+        const responseData = error.response.data;
+        setErrorMsg(responseData.code + " " + responseData.message);
+      }
+    }
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrorMsg("");
+  }, [loginState]);
+
   return (
     <div className="wrapper">
       <form className="row g-3" onSubmit={handleSubmit}>
         <h1>Login</h1>
+        <p
+          ref={errRef}
+          className={errorMsg ? "errorMsg" : "hide"}
+          aria-live="assertive"
+        >
+          {errorMsg}
+        </p>
         <InputGroup className="inputgroup mb-3">
           <Form.Control
+            type="text"
             name="username"
             placeholder="Username"
             aria-label="Username"
-            aria-describedby="basic-addon1"
             onChange={handleChange}
+            ref={userRef}
+            value={loginState.username}
+            autoComplete="off"
+            required
           />
         </InputGroup>
         <InputGroup className="inputgroup mb-3">
           <Form.Control
+            type="password"
             name="password"
             placeholder="Password"
             aria-label="Password"
-            aria-describedby="basic-addon2"
             onChange={handleChange}
+            value={loginState.password}
+            required
           />
         </InputGroup>
         <Button className="button" type="submit" variant="primary">
