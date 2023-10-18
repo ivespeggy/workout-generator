53e11689c0c323da2a87585055b7f16b1913b5aa
HaonanZhang101045166 <59615110+HaonanZhang101045166@users.noreply.github.com>
2023-10-18 17:51:37 -0400

    Carousel image effect in home page

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import "./Home.css"
+import { useState } from "react";
+import { Carousel } from "react-bootstrap";
+import "./Home.css";
+import slides from "../images/image";
 
 export default function Home() {
+  const [index, setIndex] = useState(0);
 
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
   return (
-    <div>
-      Real Home
-    </div>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      {slides.map((slide) => (
+        <Carousel.Item key={slide.image}>
+          <img className="image-style" src={slide.image} alt="First slide" />
+          <Carousel.Caption>
+            <h3>{slide.title}</h3>
+            <p>{slide.subtitle}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 }
