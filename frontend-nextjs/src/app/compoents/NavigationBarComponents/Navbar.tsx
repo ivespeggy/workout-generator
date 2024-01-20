aec429a5e5b35234204f9017d34a0eef8cd25ce6
mammut001 <dongpei110@gmail.com>
2024-01-19 20:36:18 -0500

    clean up Navbar.tsx

--- frontend-nextjs/src/app/compoents/NavigationBarComponents/Navbar.tsx
+++ frontend-nextjs/src/app/compoents/NavigationBarComponents/Navbar.tsx
@@ -16,33 +16,6 @@ const Navbar = () => {
         <>
             <nav className='flex justify-between w-full items-center text-black'>
             {NavbarStates === 'vistors' ? <NavBarVistors/> : <NavbarUsers/>}
-
-                {/* <div className='flex items-center'>
-                    <button className='pl-2' onClick={() => console.log("Home Button Clicked")}>
-                        Home1
-                    </button>
-                </div>
-
-                <div className='flex items-center'>
-                    <div className='flex flex-col items-center'>
-                        <FontAwesomeIcon icon={faUserPlus} size="sm" style={{ width: '18px', height: '18px' }} />
-                        <button className='pl-2' onClick={() => {
-                            setPopupOpen(true);
-                            setPurpose('login');
-                        }}>
-                            Login
-                        </button>
-                    </div>
-                    <div className='flex flex-col items-center'>
-                        <FontAwesomeIcon icon={faRightToBracket} size="sm" style={{ width: '18px', height: '18px' }} />
-                        <button className='pl-2' onClick={() => {
-                            setPopupOpen(true);
-                            setPurpose('signup');
-                        }}>
-                            Sign Up
-                        </button>
-                    </div>
-                </div> */}
             </nav>
             <PopupWindow isOpen={isPopupOpen} onClose={() => setPopupOpen(false)} purpose={purpose} />
         </>
