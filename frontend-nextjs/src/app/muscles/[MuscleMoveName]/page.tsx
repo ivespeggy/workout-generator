5cd60d073f8a8e40b160234f77c5530ad6bad983
mammut001 <dongpei110@gmail.com>
2024-02-02 02:12:06 -0500

    Feat: dynamic route now works
    User now can dynamically choose muscle from the list.

--- frontend-nextjs/src/app/muscles/[MuscleMoveName]/page.tsx
+++ frontend-nextjs/src/app/muscles/[MuscleMoveName]/page.tsx
@@ -8,21 +8,20 @@ import { MuscleGroupUtils } from '../../MuscleData/Muscles';
 export default function MusclePage({params}:{
     params:{MuscleMoveName:string}
 }){
-    const selectedMoveName = decodeURIComponent(params.MuscleMoveName)
-    const result = MuscleGroupUtils.searchForMuscle(selectedMoveName)
-    console.log(result)
-
+    var selectedMoveName = decodeURIComponent(params.MuscleMoveName).toLowerCase()
+    
+    console.log("Fetched Name is "+selectedMoveName)
     return (     
         <div className='flex flex-row'>
             <Image
-            src="/muscles/deltoids.png"
+            src={`/muscles/${selectedMoveName}.jpg`}
             width={200}
             height={350}
             alt="Picture of the author"
             />
 
             <strong className='p-4'>
-                Move ID: {selectedMoveName}
+                {selectedMoveName}
             </strong>
 
             
