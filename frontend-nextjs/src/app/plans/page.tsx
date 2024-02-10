edf5b4252f49b28109defc7af87450b74acfeaf4
mammut001 <dongpei110@gmail.com>
2024-02-10 11:16:22 -0500

    fix
    added unique id for <li>
    update css.

--- frontend-nextjs/src/app/plans/page.tsx
+++ frontend-nextjs/src/app/plans/page.tsx
@@ -1,17 +1,24 @@
 import { MuscleGroupUtils } from "../MuscleData/Muscles";
 const plans = ()=>{
     const daysOfWeek: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    const n = MuscleGroupUtils.generateRandomMuscleMove(1)
-    console.log(n)
+    const randomMuscles = MuscleGroupUtils.generateRandomMuscleMove(7)
+    // console.log(randomMuscles)
     return(
         <>
+        <h1 className="text-xl mb-4"><strong>Example Work Plans</strong></h1>
         <strong>
             {daysOfWeek.map(
-                day =>(
-                    <li key={day}>{day}
-                        <ul>
-                            123
-                        </ul>
+                (day,index) =>(
+                    <li key={day} className="text-base">{day} {randomMuscles[index].name_en}
+                            <ul className="text-sm text-violet-800">
+                            {randomMuscles[index].training_moves.map((move, moveIndex) =>(
+                                <li key={`${move}-${moveIndex}`}>
+                                    {move}
+                                </li>
+                            ))}
+                            </ul>
+
+
                     </li>
                     
                 )
