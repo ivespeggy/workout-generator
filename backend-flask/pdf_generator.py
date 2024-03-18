adddf258f33a1660f502fefa12144c4dbb643aed
mammut001 <dongpei110@gmail.com>
2024-03-18 18:30:45 -0400

    feat: append workout parts to the PDF NOW!!!

--- backend-flask/pdf_generator.py
+++ backend-flask/pdf_generator.py
@@ -2,15 +2,21 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import cm
 from config import PDF_FILE_NAME
+
+import datetime
+
 class PDFGenerator:
     def __init__(self, data) -> None:
         self.data = data
     
-    def parse_data(self):
+    def parse_data_daysSelected(self):
         days_selected = self.data.get('daysSelected', 'default_days_selected')
         return days_selected
     
-
+    def parse_data_workout_names(self):
+        workout_names = self.data.get('dayWorkouts','default_workout_name')
+        return workout_names
+    
 
     def generate_pdf(self, file_path=PDF_FILE_NAME):
         # create PDF canvas        
@@ -49,6 +55,33 @@ class PDFGenerator:
         c.setFont(font_times_roman, 14)
         for day in week_days:
             c.drawString(x_pos + 0.2 * cm, y_pos, day)
+            # update y postion after each iteration.
             y_pos -= cell_height
+
+        
+        # reset y
+        y_pos = y_pos_start - cell_height / 2
+
+
+        days_selected = self.parse_data_daysSelected()
+        workout_names = self.parse_data_workout_names()
+
+
+        for key, val in days_selected.items():
+            if not val:
+                # if val is false, which means user toggle off the checkbox
+                c.drawString(x_pos+ 5.2 * cm, y_pos, "REST")
+            else:
+                c.drawString(x_pos + 5.2 * cm, y_pos, workout_names[key])
+                # draw checkbox
+                c.rect(x_pos+ 8 *cm, y_pos, 15, 15)
+
+            y_pos -= cell_height
+
+        date = datetime.datetime.now()
+        formatted_date = date.strftime("%B %d, %Y")
+        print(formatted_date)
+        
+        c.drawString( 240, 100,formatted_date )
 
         c.save()
