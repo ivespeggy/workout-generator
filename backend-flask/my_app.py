fe5570f6f5614c36d11548a236436af49cce20d0
P A I <dongpei110@gmail.com>
2024-03-19 03:37:39 -0400

    feat: OTP now works.
    FAKE OTP now works though.
    Added a new route!!!!

--- backend-flask/my_app.py
+++ backend-flask/my_app.py
@@ -1,10 +1,29 @@
+import os
 from flask import Flask, send_file, request, jsonify
 from reportlab.pdfgen import canvas
 from flask_cors import CORS
 from pdf_generator import PDFGenerator
 from config import PDF_FILE_NAME
+from otp import OTPAuth
+from dotenv import load_dotenv
+
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
+
+sender_email = os.getenv('TEST_EMAIL')
+password = os.getenv('PASSWORD')
+recipient = os.getenv('TEST_RECP')
+
+
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    otp = OTPAuth(sender_email, password)
+    status_code = otp.send_email("123456", recipient)
+    if status_code["status"]:
+        return jsonify({'message': 'OTP sent successfully'}), 200
+    else:
+        return jsonify({'error' + status_code["msg"]}), 500
 
 @app.route('/generate_pdf', methods=['POST'])
 def generate_pdf():
---
+++ backend-flask/otp.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+class OTPAuth:
+    def __init__(self, sender_email, sender_password,smtp_server = "smtp.gmail.com", port = 587) -> None:
+        self.sender_email = sender_email
+        self.sender_password = sender_password
+        self.smtp_server = smtp_server
+        self.port = port
+    
+    def send_email(self, otp, recipient)->dict:
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "OTP"
+        message["From"] = self.sender_email
+        message["To"] = recipient
+
+        text = f"""\
+        Hi,
+        Your OTP is: {otp}
+        """
+        part = MIMEText(text, "plain")
+        message.attach(part)
+
+        try:
+            server = smtplib.SMTP(self.smtp_server, self.port)
+            server.starttls()  # Secure the connection
+            server.login(self.sender_email, self.sender_password)
+            server.sendmail(self.sender_email, recipient, message.as_string())
+            server.quit()
+            print("OTP sent successfully.")
+            return {"status" : True, "msg":"OTP sent successfully."}
+        except Exception as e:
+            print(f"Failed to send OTP. Error: {e}")
+            return {"status": False, "msg":str(e)}
+
