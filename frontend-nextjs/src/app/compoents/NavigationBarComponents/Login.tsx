8e5d4291bfbaefbf76e19eca376f56d6231edf8e
mammut001 <dongpei110@gmail.com>
2024-03-31 00:46:22 -0400

    fix: fixed overlapping issue by changing z-index of poupup window

--- frontend-nextjs/src/app/compoents/NavigationBarComponents/Login.tsx
+++ frontend-nextjs/src/app/compoents/NavigationBarComponents/Login.tsx
@@ -27,17 +27,11 @@ const Login = () => {
                         type="text"
                         name="username"
                         placeholder="Username"
+                        className="p-2 w-full border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 mt-2 mb-2"
                         // onChange={handleChange}
                     />
                 </div>
-                <div className="input-group">
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        // onChange={handleChange}
-                    />
-                </div>
+
                 <button type="submit" className="flex justify-center items-center px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700">
                     Submit
                 </button>
