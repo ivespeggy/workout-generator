edef3bc1e033cc7f52a572cf3ed016d4626eac70
mammut001 <dongpei110@gmail.com>
2024-04-09 22:32:27 -0400

    final push for this PR

--- frontend-nextjs/src/app/compoents/PlansComponents/Selector.tsx
+++ frontend-nextjs/src/app/compoents/PlansComponents/Selector.tsx
@@ -1,28 +1,74 @@
-import { useDaysInWeekStore } from "../../store/usePlanStore"
+'use client'
+import { MuscleGroupUtils } from "../../MuscleData/Muscles"
+import { useEffect } from "react"
+import { useDaysInWeekStore, useMuscleGroupStore } from "../../store/usePlanStore"
 const Selector = ()=>{
     const daysOfWeek = useDaysInWeekStore(state => state.daysOfWeek)
-    console.log("11132")
-    console.log(daysOfWeek)
-    console.log("11132")
 
-    // Object.keys(musclesData).forEach((k)=>{
-    //     const key = parseInt(k,10)
-    //     const muscleGroup = musclesData[key]
-    //     res.push(muscleGroup.name_en)
-    //     // console.log(`English: ${muscleGroup.name_en}, Chinese: ${muscleGroup.name_cn}`);
-    //   })
+
+    const allMoves = useMuscleGroupStore(state=>state.muscles)
+    const setMoveStatus = useMuscleGroupStore(state => state.toggleMuscle)
+
+    const dayOnSelectMuscle = useMuscleGroupStore(state => state.dayOnSelectMuscle)
+    const updateDayOnSelectItem = useMuscleGroupStore(state => state.updateDayOnSelectMuscle)
+    const deleteDayOnSelectItem = useMuscleGroupStore(state => state.deleteCurrMuscleOnSelect)
+
+    const handleOnSelect = (weekId:string, event:React.ChangeEvent<HTMLSelectElement>) =>{
+        const selectedMuscle = event.target.value
+
+        console.log("Week ID IS "+weekId + " moveNameKey " +selectedMuscle)
+
+        if (event.target.value !== "default"){
+            if (dayOnSelectMuscle[weekId] === ""){
+                // If dayOnSelectMuscle['M'] === "" means they're empty, so we can just call update function here to update OnSelect Item.
+                updateDayOnSelectItem(weekId,selectedMuscle)
+                setMoveStatus(selectedMuscle)
+            }
+            else{
+                // If dayOnSelectMuscle['M'] === "Some Muscles", then we have to delete current muscle.
+                const currMuscle = dayOnSelectMuscle[weekId]
+                deleteDayOnSelectItem(weekId,currMuscle)
+                updateDayOnSelectItem(weekId,selectedMuscle)
+
+                //Firstly, toggle current muscle state to false, means it is not longer selected.
+                setMoveStatus(currMuscle)
+                setMoveStatus(selectedMuscle)
+                
+
+            }
+
+            console.log("All MOVES :")
+            console.log(allMoves)
+            console.log("END OF THE PRINT")
+
+        }
+        // update in React DEBUGER, not print out in console.
+
+
+
+    }
+
+// This renders all the moves option for each day */
+
+
     return (
         <div>
+            {/* this for loop only works when v is true, v is the everyday pixel  */}
         {Object.entries(daysOfWeek).map(([key, v]) => (
-            v===true?(
+            v === true?(
                 <div key={key}>
                 Keys: {key}, Value: {v.toString()}
                 <div>
-                    <select name="workout-selection">
-                        <option value="">--Please choose an option--</option>
-                        <option value="dog">Dog</option>
-                        <option value="cat">Cat</option>
-                            
+                    <select name="workout-selection" value={dayOnSelectMuscle[key]} onChange={(event) => handleOnSelect(key, event)}>
+                    <option value="default">--Please choose an option--</option>
+
+                    {Object.entries(allMoves).map(([muscle,isSelected]) =>{
+                        if (dayOnSelectMuscle[key] === muscle || !isSelected) {
+                            return <option key={muscle} value={muscle}>{muscle}</option>
+                        }
+                        return null
+                    })                            
+                    }
                     </select>
                 </div>
               </div>
--- frontend-nextjs/src/app/store/usePlanStore.ts
+++ frontend-nextjs/src/app/store/usePlanStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { MuscleGroupUtils } from "../MuscleData/Muscles";
 
 type PixelSquareStore = {
     OnSelectStatus: boolean
@@ -15,6 +16,68 @@ type DaysInWeekStore = {
     toggleDay:(initial:string) => void
 }
 
+
+
+type SelectorStore = {
+    selectedOption: string,
+    updateSelectedOption:(s:string) => void
+}
+export const useSelectorStore = create<SelectorStore>() ((set)=>({
+    selectedOption:'',
+    updateSelectedOption: (inputS:string) => set({selectedOption:inputS})
+}))
+
+type MuslceGroupStore = {
+    updateDayOnSelectMuscle:(dayInitial:string,muscleName:string) => void,
+    deleteCurrMuscleOnSelect:(dayInitial:string, muscleName:string) => void,
+    dayOnSelectMuscle: {[key:string]:string},
+    muscles: {[key: string]:boolean},
+    toggleMuscle:(muscleName:string) => void
+}
+// type OnSelectOption = {
+//     selectorMuscles:{[key:string]:string},
+//     setMusscle: (id:string, m:string) =>void
+
+// }
+// export const useOnSelectMuscleOption = create<OnSelectOption>() ((set)=>({
+//     selectorMuscles:{'M':'','T':'', 'W':'', 'R':'','F':'','S':'','U':''},
+//     setMusscle: (id:string,m:string) => set((state) =>{
+//         const newSelectorMuscles = {
+//             ...state.selectorMuscles,
+//             [id]:m
+//         }
+//         console.log("正在更新 selectorMuscles:", newSelectorMuscles);
+//         return {selectorMuscles:newSelectorMuscles}
+//     })
+// }))
+
+export const useMuscleGroupStore = create<MuslceGroupStore>() ((set)=>({
+    deleteCurrMuscleOnSelect: (dayInitial:string, muscleName:string) => set((state) =>{
+        const newDayOfOnSelectMuscle = {
+            ...state.dayOnSelectMuscle,
+            [dayInitial]:''
+        }
+        return {dayOnSelectMuscle:newDayOfOnSelectMuscle}
+    }),
+    updateDayOnSelectMuscle: (dayInitial:string, muscleName:string) => set((state)=>{
+        const newDayOfOnSelectMuscle = {
+            ...state.dayOnSelectMuscle,
+            [dayInitial]:muscleName
+        }
+        return {dayOnSelectMuscle:newDayOfOnSelectMuscle}
+    }),
+    dayOnSelectMuscle: {'M':'','T':'', 'W':'','R':'','F':'','S':'','U':''},
+    muscles: MuscleGroupUtils.getAllMoveName(),
+
+    toggleMuscle: (muscleName:string) => set((state) =>{
+        const selectedMuscleState = state.muscles[muscleName]
+        const newMuscles = {
+            ...state.muscles,
+            [muscleName]: !selectedMuscleState,
+        }
+        return {muscles:newMuscles}
+    })
+}))
 
 export const useDaysInWeekStore = create<DaysInWeekStore>() ((set,get)=>({
     daysOfWeek:{'M':true,'T':true,'W':true,'R':true,'F':true,'S':true,'U':true},
